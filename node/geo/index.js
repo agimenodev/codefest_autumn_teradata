831c6e8224b8d5ba82c50c039e85ab5b0f2e477f
Uros Lates <uros.lates@teradata.com>
2015-12-07 16:55:01 +0100

    Adding express server

---
+++ node/geo/index.js
@@ -0,0 +1,57 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');
+var bodyParser = require('body-parser');
+var app        = express();
+var morgan     = require('morgan');
+
+// configure app
+app.use(morgan('dev')); // log requests to the console
+
+// configure body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port     = process.env.PORT || 8080; // set our port
+
+var mongoose   = require('mongoose');
+var DB_NAME = 'geo';
+mongoose.connect('mongodb://localhost:27017/' + DB_NAME); // connect to our database
+var Country     = require('./app/models/country');
+
+// ROUTES FOR OUR API
+// =============================================================================
+
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next();
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+
+// on routes that end in /geo/country
+// ----------------------------------------------------
+router.route('/geo/country')
+
+// on routes that end in /geo/country/:country_id
+// ----------------------------------------------------
+router.route('/geo/country/:country_id');
+
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
